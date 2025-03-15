33f0a79870146bd9a8abfd4797e7de857887cf77
EEBros1 <edebo4ican@gmail.com>
2025-03-15 17:20:03 +0000

    Add files via upload

---
+++ Identity Operator.py
@@ -0,0 +1,12 @@
+Number1 = 8
+if  (type(Number1) is int) :
+    print ("8 is an integer")
+else :
+    print ("8 is not an integer")#
+
+PositionX = 23
+PositionY = -17
+if (PositionX is PositionY) :
+    print ("PositionX and PositionY are the same number")
+else :
+    print ("PositionX and PositionY are not the same number")
